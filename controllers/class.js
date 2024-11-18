8f63a4e48bb8a967909c38ca1d6cec398b09617c
coderXgourav <softwaredevelopergourav@gmail.com>
2024-11-18 23:51:39 +0530

    class edit page commit

--- controllers/class.js
+++ controllers/class.js
@@ -63,6 +63,57 @@ const getSection = async(req,res) =>{
     }
 }
 
+const getClassDetails = async(req,res) =>{
+    const id = req?.params?.id;
+    try{
+        const data = await classModel.findById(id);
+        const section = data;
+        return res.status(200).send(section);
+    }catch(error){
+        return res.status(500).json({status:false,title:"Technical Issue",desc:error.message});
+    }
+}
+
+const updateClass = async(req,res) =>{
+    const id = req?.body?.classId;
+    let {className,sections} = req.body;
+
+    // for(let i=0; i<sections.length; i++){
+    //     console.log(sections[i].sectionName);
+    //     console.log(sections[i]._id);
+    // }
+    // return;
+    className = className.toUpperCase();
+    if(!className ||sections.length<1){
+        return res.status(400).json({status:false,title:"Fill Required Fields",desc:"Please Fill all the fields"});
+    }
+    try{ 
+    let classes = await classModel.findById(id);
+  if(classes){
+    const checkExist = await classModel.find({'class_name':className});
+    if(checkExist.length<2){
+    const checkExist = await classModel.findByIdAndUpdate(id,{'class_name':className});
+    return res.status(200).json({status:true,title:"Class Updated",desc:"Class Has been updated!"});
+
+    }else{
+    return res.status(400).json({status:false,title:"Class Already Exist",desc:"This class already exist in your record!"});
+    }
+  }else{
+    return res.status(400).json({status:false,title:"Invalid Class Id",desc:"You are sent invalid class id!"});
+
+  }
+    
+
+    console.log(classes);
+          
+    }catch(error){
+        return res.status(500).json({status:false,title:"Techinal Issue!",desc:error.message});
+    }
 
 
-module.exports ={addClass,getClass,deleteClass,getSection}
+
+}
+
+
+
+module.exports ={addClass,getClass,deleteClass,getSection,getClassDetails,updateClass}
--- routes/class.js
+++ routes/class.js
@@ -1,14 +1,16 @@
 const {Router} = require('express');
 const {verifyToken} = require("../middlewares/verifyToken");
 const classRouter = Router();
-const {addClass,getClass,deleteClass,getSection} = require("../controllers/class");
+const {addClass,getClass,deleteClass,getSection,getClassDetails,updateClass} = require("../controllers/class");
 
 
 classRouter.get('/get-class',verifyToken, getClass);
 
+classRouter.put('/update-class',verifyToken,updateClass);
 classRouter.post('/add-class',verifyToken,addClass);
 classRouter.delete('/delete/:id',verifyToken,deleteClass);
 classRouter.get('/get-section/:id',verifyToken,getSection);
+classRouter.get('/:id',verifyToken,getClassDetails);
 
 
 module.exports = classRouter;
